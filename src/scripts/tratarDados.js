ed45fa29a08b9ee8098edea98ba6a0fc2d570747
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2024-12-17 17:11:34 -0300

    Melhoria time stamps

--- src/scripts/tratarDados.js
+++ src/scripts/tratarDados.js
@@ -22,11 +22,14 @@ function doThePositions(valores, grandezas, preferredGrandeza) {
     return positions.sort((value1, value2) => value1-value2); 
 }
 
-function mesclarInString(list1, list2, intercalation, end) {
+function mesclarInString(list1, list2, intercalation, end, doMoreOneThing) {
     let res = []
 
     for(let i = 0; i < list1.length; i++) {
         res.push(`${list1[i]}${intercalation}${list2[i]}${end}`);
+        if(doMoreOneThing !== undefined) {
+            doMoreOneThing(list1, list2, i, res);
+        }
     }
 
     return res; 
@@ -34,9 +37,11 @@ function mesclarInString(list1, list2, intercalation, end) {
 
 const resultados = document.querySelector(".resultados")
 
+let valores; 
+
 botao_positions.addEventListener("click", () => {
     if(passed === 1) {
-        const valores = {
+        valores = {
             velocity: (new Velocity(toNumber(document.getElementById("velocity").value), document.querySelector('#velocidade-grandeza').value)).mbys,
             time: (new Time(toNumber(document.getElementById("tempo-de-abertura").value), document.getElementById("tempo-de-abertura-grandeza").value)).seconds,
             s_0: (new Space(toNumber(document.getElementById("posicao-inicial").value), document.getElementById("posicao-inicial-grandeza").value)).metros,
@@ -44,26 +49,33 @@ botao_positions.addEventListener("click", () => {
             positions: doThePositions(document.querySelectorAll(".input-positions"), document.querySelectorAll(".posicoes-grandeza"), "metros")
         }
 
-        console.log(valores);
     
         let times = calcMultiplePositions(valores.positions, valores.s_0, 0, valores.velocity);
         let timesDiference = calcMultiplePositions(valores.positions, valores.s_0, 1, valores.velocity);
+
         const trajetoPlace = document.querySelector(".trajeto");
         const temposDiferenciaisPlace = document.querySelector(".tempos-diferenciais");
         const timeText = trajetoPlace.querySelector(".time");
-        times = mesclarInString(times, valores.positions, ' segundo(s): ', ' metro(s)');
+        const paragrafoAteSemaoforo = document.querySelector(".ate-semaforo");
+
+        times = mesclarInString(times, valores.positions, ' segundo(s): ', ' metro(s)', (timeList, positionlist, idx, resList) => {
+            if(timeList[idx] < 0) {
+                resList[idx] = `O carro não passara nesse semáforo, posição dele: ${positionlist[idx]} metro(s)`;
+            }
+        });
         timeText.innerHTML = times[0];
     
         const timeStamps = new ElementosInputs('<div class="traco"></div><div class="time-point"><div class="bullet-point"></div><p class="time">{value}</p></div>');
-        const timesDiferences = new ElementosInputs('<div class="time-point-distance"><div class="traco-info"></div><p class="diferenca-tempo">{value} segundos</p></div>')
-    
+        const timesDiferences = new ElementosInputs('<p class="diferenca-tempo">{value} segundos</p></div>')
+        
+        paragrafoAteSemaoforo.innerHTML = `Para chegar ao primeiro semáforo da posição inicial demorou ${timesDiference[0]} segundo(s).`
         trajetoPlace.innerHTML += timeStamps.doOnList(times.slice(1, times.length), "value");
         temposDiferenciaisPlace.innerHTML += timesDiferences.doOnList(timesDiference.slice(1, timesDiference.length), "value")
     
         resultados.classList.add("appear")
         popupPositions.classList.remove("appear");
         verificarClick(".resultados", `#${botao_positions.id}`, () => {
-            trajetoPlace.innerHTML = '<div class="trajeto"><div class="time-point"><div class="bullet-point"></div><p class="time"></p></div></div>';
+            trajetoPlace.innerHTML = '<div class="time-point"><div class="bullet-point"></div><p class="time"></p></div>';
             temposDiferenciaisPlace.innerHTML = "";
         });
     }
